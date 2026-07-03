f70204d6df4d413bd1420e52930055ee2132d893
LukeHsiao-YC <u9602041@gmail.com>
2026-07-03 21:57:43 +0800

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ const articlesCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     journal: z.string(),
+    category: z.string().default('未分類'), // 新增這一行
     pubDate: z.string(),
     link: z.string(),
     tags: z.array(z.string()).default([]),
