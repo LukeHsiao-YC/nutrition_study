c9dce67a9bff41f18f9445dd7736bfe30f02eb20
LukeHsiao-YC <u9602041@gmail.com>
2026-07-05 13:01:01 +0800

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  output: 'static',
+  site: 'https://LukeHsiao-YC.github.io', // 請改成你的實際網址
+  base: '/nutrition_study/' // 請改成你的實際專案名稱
+});
